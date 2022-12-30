33bb762c95672c3d8638a1b28f43bef220b5af3f
Baheya Khalifa <khalifa.baheya@gmail.com>
2022-12-30 15:51:47 +0200

    connected Storyblok CMS

---
+++ src/utils/storyblok.js
@@ -0,0 +1,56 @@
+import {
+  storyblokInit,
+  apiPlugin,
+  storyblokEditable,
+  loadStoryblokBridge,
+} from '@storyblok/js'
+ 
+const dev = import.meta.env.DEV
+const storyblokToken = import.meta.env.STORYBLOK_ACCESS_TOKEN
+ 
+const { storyblokApi } = storyblokInit({
+  accessToken: storyblokToken,
+  apiOptions: {
+    cache: { type: 'memory' },
+  },
+  use: [apiPlugin],
+})
+ 
+export async function getLinks() {
+  const { data } = await storyblokApi.get('cdn/links', {
+    version: 'draft',
+  })
+ 
+  const links = data ? data.links : null
+ 
+  return links
+}
+ 
+export async function getStory(slug) {
+  const { data } = await storyblokApi.get(`cdn/stories/${slug}`, {
+    version: 'draft',
+  })
+ 
+  const story = data ? data.story : null
+ 
+  return story
+}
+
+export async function getEditableAttributes(blok) {
+  if (!blok || !dev) return false
+ 
+  return storyblokEditable(blok)
+}
+ 
+export async function activateStoryblokBridge() {
+  if (!dev) return false
+ 
+  return loadStoryblokBridge().then(() => {
+    const { StoryblokBridge, location } = window
+    const storyblokInstance = new StoryblokBridge()
+ 
+    storyblokInstance.on(['published', 'change'], () => {
+      location.reload(true)
+    })
+  })
+}
